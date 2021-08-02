989a45c4c81dfd336203fa8faa62ca11be4b010e
carolina fuentes <fuecarolina@gmail.com>
2021-08-02 19:15:37 -0400

    adding autor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_223215) do
+ActiveRecord::Schema.define(version: 2021_08_02_230929) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_02_223215) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "autor"
   end
 
 end
